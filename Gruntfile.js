35b07739e57f90ab80df9301f9771de935769f96
ilyaoki <ilyaoki@gmail.com>
2016-02-19 19:53:36 +0300

    gruntfile changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,25 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+
         shell: {
-            options: {
-                stdout: true,
-                stderr: true
+            dev: {
+                command: 'node server.js'
             },
-            server: {
-                command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 8080'
-            }
         },
+
+        watch: {
+            fest: {
+                files: 'templates/*.xml',
+                tasks: ['fest']
+            },
+            
+        },
+        
+        concurrent: {
+            start:  ['shell', 'watch']
+        },
+
         fest: {
             templates: {
                 files: [{
@@ -27,39 +37,16 @@ module.exports = function (grunt) {
                     }
                 }
             }
-        },
-        watch: {
-            fest: {
-                files: ['templates/*.xml'],
-                tasks: ['fest'],
-                options: {
-                    interrupt: true,
-                    atBegin: true
-                }
-            },
-            server: {
-                files: [
-                    'public_html/js/**/*.js',
-                    'public_html/css/**/*.css'
-                ],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        concurrent: {
-            target: ['watch', 'shell'],
-            options: {
-                logConcurrentOutput: true
-            }
         }
+
     });
 
+    // подключть все необходимые модули
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-fest');
 
-    grunt.registerTask('default', ['concurrent']);
-
+    // результат команды grunt
+    grunt.registerTask('default', ['shell', 'watch']);
 };
